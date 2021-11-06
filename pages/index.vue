d0881746a7f636b13683c6d65bb65ebbc7213688
jiahong96 <jiahong9705@gmail.com>
2021-11-06 23:45:16 +0800

    feat: load data based on selection

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,42 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-col cols="12" sm="10" md="8">
+      <WeatherCard
+        v-if="weatherData !== null"
+        :city-name="city"
+        :weather-data="weatherData"
+      />
     </v-col>
   </v-row>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      weatherData: null,
+      loading: false
+    }
+  },
+  computed: {
+    city () {
+      return this.$store.state.weather.queryString
+    }
+  },
+  watch: {
+    city (newValue) {
+      this.queryWeather(newValue)
+    }
+  },
+  methods: {
+    async queryWeather (city) {
+      this.loading = true
+
+      const response = await this.$axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.$config.weatherApiKey}`)
+
+      this.loading = false
+      this.weatherData = response.data
+    }
+  }
+}
+</script>

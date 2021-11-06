710fa0fac3bae37b619d6818563e55db1ba4c46c
jiahong96 <jiahong9705@gmail.com>
2021-11-06 14:43:29 +0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-btn
+        icon
+      >
+        <v-icon>mdi-weather-cloudy</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="true"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }} {{ author }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Weather Forecast',
+      author: 'Loo Cheah Hong'
+    }
+  }
+}
+</script>

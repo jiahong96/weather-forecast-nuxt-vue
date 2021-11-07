d95c5d10441a90662e3713b9b1728f069bd2fa28
jiahong96 <jiahong9705@gmail.com>
2021-11-07 22:41:13 +0800

    feat: display forecast data

--- components/WeatherCard.vue
+++ components/WeatherCard.vue
@@ -22,7 +22,7 @@
         </v-col>
         <v-col cols="6">
           <v-img
-            :src="icon"
+            :src="mainIcon"
             :alt="description"
             width="92"
           />
@@ -39,6 +39,29 @@
       <v-list-item-title>Humidity</v-list-item-title>
       <v-list-item-subtitle> {{ humidity }}%</v-list-item-subtitle>
     </v-list-item>
+
+    <v-divider />
+
+    <v-list class="transparent">
+      <v-list-item
+        v-for="(item, key) in forecastDates"
+        :key="key"
+      >
+        <v-list-item-title>{{ key }}</v-list-item-title>
+
+        <v-list-item-icon>
+          <v-img
+            :src="iconUrl(item.icon)"
+            alt="weather icon"
+            width="70"
+          />
+        </v-list-item-icon>
+
+        <v-list-item-subtitle class="text-right">
+          {{ `${temperatureConverter(item.tempMax)}&deg;C / ${temperatureConverter(item.tempMin)}&deg;C` }}
+        </v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
   </v-card>
 </template>
 
@@ -48,9 +71,34 @@ export default {
     weatherData: {
       type: Object,
       required: true
+    },
+    forecastData: {
+      type: Object,
+      required: true
     }
   },
   computed: {
+    forecastDates () {
+      return (this.forecastData.list || []).reduce((accumulated, item) => {
+        const date = new Date(item.dt_txt)
+        const day = date.toLocaleDateString('en-US', { weekday: 'long' })
+
+        if (!accumulated[day]) {
+          accumulated[day] = { tempMin: item.main.temp_min, tempMax: item.main.temp_max, icon: item.weather[0].icon }
+        } else {
+          if (accumulated[day].tempMin > item.main.temp_min) {
+            accumulated[day].tempMin = item.main.temp_min
+          }
+
+          if (accumulated[day].tempMax < item.main.temp_max) {
+            accumulated[day].tempMax = item.main.temp_max
+            accumulated[day].icon = item.weather[0].icon
+          }
+        }
+
+        return accumulated
+      }, {})
+    },
     cityName () {
       return this.weatherData.name || ''
     },
@@ -58,7 +106,7 @@ export default {
       return this.weatherData.weather?.[0]?.description || ''
     },
     celsius () {
-      return Math.round(parseFloat(this.weatherData.main?.temp) - 273.15)
+      return this.temperatureConverter(this.weatherData.main?.temp)
     },
     humidity () {
       return this.weatherData.main?.humidity || 'N/A'
@@ -72,8 +120,16 @@ export default {
 
       return today.toLocaleString('en-US', options)
     },
-    icon () {
-      return `https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/${this.weatherData.weather?.[0]?.icon}.png`
+    mainIcon () {
+      return this.iconUrl(this.weatherData.weather?.[0]?.icon)
+    }
+  },
+  methods: {
+    temperatureConverter (tempInKelvin) {
+      return Math.round(parseFloat(tempInKelvin) - 273.15)
+    },
+    iconUrl (iconId) {
+      return `https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/${iconId}.png`
     }
   }
 }
